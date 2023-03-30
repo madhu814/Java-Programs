7dd243a5809432d63d775b559acf0aad731ef94b
Madhubala <madhusingh1867@gmail.com>
2023-03-31 01:52:27 +0530

    Returning sum of two integers

---
+++ ReSum.java
@@ -0,0 +1,16 @@
+public class ReSum
+{
+    public static int sum()
+    {
+        int a,b;
+        a=20;
+        b=3;
+        int sum=a+b;
+        return sum;
+    }
+public static void main(String[] args)
+{
+    ReSum rs=new ReSum();
+    System.out.println(rs.sum());
+}
+}
