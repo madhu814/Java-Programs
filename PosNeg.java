b29e996406226755cde84d70d85a34319ba3c7cd
Madhubala <madhusingh1867@gmail.com>
2023-03-31 00:55:38 +0530

    Java Boot champ Program

---
+++ PosNeg.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class PosNeg 
+{
+    public static void main(String[] args)
+    {
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter a number");
+        int i=s.nextInt();
+        if(i>0)
+        {
+            System.out.println("The number is positive");
+        }
+        else
+        System.out.println("The number is negative");
+    }
+    
+}
